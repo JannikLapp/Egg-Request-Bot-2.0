f95d05404eff3c45e5c3ce6452cd947f7b5b8416
Jannik-PC <jannik.lapp@mni.thm.de>
2022-03-09 18:26:38 +0100

    added My-SQL Support

--- src/main/java/com/eqqrequestbot2/eggrequestbot2/controller/UserController.java
+++ src/main/java/com/eqqrequestbot2/eggrequestbot2/controller/UserController.java
@@ -1,9 +1,35 @@
 package com.eqqrequestbot2.eggrequestbot2.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.eqqrequestbot2.eggrequestbot2.model.User;
+import com.eqqrequestbot2.eggrequestbot2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam Integer discordId) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setDiscordId(discordId);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
 
 }
---
+++ src/main/java/com/eqqrequestbot2/eggrequestbot2/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eqqrequestbot2.eggrequestbot2.repository;
+
+import com.eqqrequestbot2.eggrequestbot2.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
